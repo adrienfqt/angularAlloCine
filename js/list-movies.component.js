51d7ac825b039a31042e2125a6c419937acd64cf
afouq <afouquet67@gmail.com>
2024-02-13 16:42:47 +0100

    affichage complet des données / Notes

--- js/list-movies.component.js
+++ js/list-movies.component.js
@@ -14,8 +14,8 @@ component('listMovies',{
         '                    <div style="width: 50px; height: 50px;">\n' +
         '                    </div>\n' +
         '                    <div>\n' +
-        '                        <span>Note Moyenne: </span>\n' +
-        '                        <span>Nombre de notes: </span>\n' +
+        '                        <span>Note Moyenne: {{$ctrl.moyenne(movie.notes)}}</span><br>\n' +
+        '                        <span>Nombre de notes: {{movie.notes.length}} </span>\n' +
         '                    </div>\n' +
         '                </article>\n' +
         '            </section>\n' +
@@ -25,7 +25,7 @@ component('listMovies',{
         this.movies = [{
             titre:'Titanic',
             description:'Bâteau qui coule, film qui fait carton plein',
-            notes:[3.00,4.55,2.43,2.5,5.00,4.5]
+            notes:[3.00,4.55,2.43,2.5,5.00,4.5,3.28]
         },{
             titre:'American Nightmare',
             description:'Film américain',
@@ -40,5 +40,14 @@ component('listMovies',{
             notes:[3.00,4.55,2.43,2.5,5.00,4.5]
         }
         ];
+        this.moyenne = function (notes) {
+            let sum=0;
+            let moyenne=0;
+            notes.forEach(note => {
+                sum += note;
+            });
+            return (sum/notes.length).toFixed(2);
+        }
+
     }
 });
