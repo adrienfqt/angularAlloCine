c18d84bbb151c8f33d3d8351c3325fae1f60eebc
SatyaMinguez <72600190+SatyaMinguez-dev@users.noreply.github.com>
2024-02-14 11:12:24 +0100

    formulaire visible

--- js/connexion.js
+++ js/connexion.js
@@ -1,8 +1,10 @@
 angular.
 module('alloCine').controller('formControlleur', function ($scope ) {
+    $scope.formVisible = true; // formulaire qui est visible
     $scope.soumettre = function(){
 
         localStorage.setItem('utilisateur',JSON.stringify($scope.utilisateur))
         console.log($scope.utilisateur);
+        $scope.formVisible = false; // le formulaire disparait
     }
 });
